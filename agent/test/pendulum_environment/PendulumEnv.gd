444c0ba079cb193366785b4561f18606d60354c4
errata-c <erratachem@gmail.com>
2022-01-26 09:35:10 -0500

    Changed the sprites, improved design document

--- agent/test/pendulum_environment/PendulumEnv.gd
+++ agent/test/pendulum_environment/PendulumEnv.gd
@@ -5,11 +5,11 @@ var pendulum_reset_position = Vector2()
 var arm_length = 0
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pendulum_reset_position = $Pendulum/Arm.position
 	arm_length = ($Pendulum/Arm.position - $Pendulum/Pin.position).length() + 0.005
 
+# Method to compute the observation for the scene, not a part of the interface
 func compute_observation(Env):
 	var arm = $Pendulum/Arm.position - $Pendulum/Pin.position
 	var x = clamp(arm.x / arm_length, -1.0, 1.0)
@@ -20,12 +20,14 @@ func compute_observation(Env):
 	Env.reward = y
 	Env.done = false
 
+# Interface method, called by the EnvironmentNode
 func step(Env):
 	compute_observation(Env)
 	
 	var force = Env.get_action("force")
 	$Pendulum/Arm.angular_velocity += force
 
+# Interface method, called by the EnvironmentNode
 func reset(Env):
 	$Pendulum/Arm.angular_velocity = 0
 	$Pendulum/Arm.linear_velocity = Vector2(0,0)
